692797f46f06aa7d24c726d05c8261fd924e6873
Zhang Yabin 张亚斌 (690) <zhangyabin.xian@haier.com>
2019-10-23 10:43:11 +0800

    init commit

---
+++ lint/src/main/groovy/com/gradle/customize/lint/LintPlugin.groovy
@@ -0,0 +1,12 @@
+package com.gradle.customize.lint
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class LintPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+
+    }
+}
